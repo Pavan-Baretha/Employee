cab805bed492b732831a734ea724c6fc7e2fd41e
Pawan Baretha <pawanbaretha7@gmail.com>
2021-04-30 18:15:18 +0530

    react bakend with spring boot.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ListEmployeeComponenet from "./componenets/ListEmployeeComponenet"
+import HeaderComponent from './componenets/HeaderComponent';
+import FooterComponent from './componenets/FooterComponent';
+import CreateEmployeeComponenet from './componenets/CreateEmployeeComponenet';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+          <HeaderComponent />
+          <div className="container">
+            <Switch>
+              <Route path="/" exact component = {ListEmployeeComponenet}></Route>
+              <Route path="/employees" component = {ListEmployeeComponenet}></Route>
+              <Route path="/add-employee" component = {CreateEmployeeComponenet}></Route>
+            </Switch>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
+
   );
 }
 
